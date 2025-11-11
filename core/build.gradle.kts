8298e30318a2fc30f9cd4c88a713d9646d9c872b
david <mail@nonswag.dev>
2025-11-11 22:31:35 +0100

    Add JetBrains annotations dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 
 dependencies {
     compileOnlyApi("com.google.code.gson:gson:2.13.2")
+    compileOnlyApi("org.jetbrains:annotations:26.0.2-1")
     compileOnlyApi("org.jspecify:jspecify:1.0.0")
 
     testImplementation("com.google.code.gson:gson:2.13.2")
