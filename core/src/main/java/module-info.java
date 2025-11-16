52d522928a6163e1e4c118e7a694f4acd7cd7cec
david <mail@nonswag.dev>
2025-11-16 13:52:39 +0100

    Update module-info with required dependencies

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -5,10 +5,10 @@ module org.faststats.core {
     exports org.faststats.chart;
     exports org.faststats;
 
+    requires com.github.luben.zstd_jni;
     requires com.google.gson;
     requires java.net.http;
 
+    requires static org.jetbrains.annotations;
     requires static org.jspecify;
-    requires org.jetbrains.annotations;
-    requires com.github.luben.zstd_jni;
 }
